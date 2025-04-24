6f61a1b8d83bae67474f779713b17df5af59b0ea
ferrannavarrete <ferrannavarretepiza@gmail.com>
2025-04-24 13:04:32 +0200

    Update node.py

--- node.py
+++ node.py
@@ -3,8 +3,8 @@ import math
 class Node:
     def __init__(self, name, x, y):
         self.name = name
-        self.x = float(x)
-        self.y = float(y)
+        self.x = x
+        self.y = y
         self.neighbors = []
 
 def AddNeighbor(n1, n2):
@@ -14,6 +14,4 @@ def AddNeighbor(n1, n2):
     return True
 
 def Distance(n1, n2):
-    dx = n2.x - n1.x
-    dy = n2.y - n1.y
-    return math.sqrt(dx**2 + dy**2)
+    return math.sqrt((n1.x - n2.x)**2 + (n1.y - n2.y)**2)
