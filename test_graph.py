081139ff5885e66917de375e9ac43b76cfdddc0b
ferrannavarrete <ferrannavarretepiza@gmail.com>
2025-04-24 13:02:15 +0200

    Update test_graph.py

--- test_graph.py
+++ test_graph.py
@@ -1,87 +1,81 @@
 from graph import *
-def CreateGraph_1 ():
- G = Graph()
- AddNode(G, Node("A",1,20))
- AddNode(G, Node("B",8,17))
- AddNode(G, Node("C",15,20))
- AddNode(G, Node("D",18,15))
- AddNode(G, Node("E",2,4))
- AddNode(G, Node("F",6,5))
- AddNode(G, Node("G",12,12))
- AddNode(G, Node("H",10,3))
- AddNode(G, Node("I",19,1))
- AddNode(G, Node("J",13,5))
- AddNode(G, Node("K",3,15))
- AddNode(G, Node("L",4,10))
- AddSegment(G, "AB","A","B")
- AddSegment(G, "AE","A","E")
- AddSegment(G, "AK","A","K")
- AddSegment(G, "BA","B","A")
- AddSegment(G, "BC","B","C")
- AddSegment(G, "BF","B","F")
- AddSegment(G, "BK","B","K")
- AddSegment(G, "BG","B","G")
- AddSegment(G, "CD","C","D")
- AddSegment(G, "CG","C","G")
- AddSegment(G, "DG","D","G")
- AddSegment(G, "DH","D","H")
- AddSegment(G, "DI","D","I")
- AddSegment(G, "EF","E","F")
- AddSegment(G, "FL","F","L")
- AddSegment(G, "GB","G","B")
- AddSegment(G, "GF","G","F")
- AddSegment(G, "GH","G","H")
- AddSegment(G, "ID","I","D")
- AddSegment(G, "IJ","I","J")
- AddSegment(G, "JI","J","I")
- AddSegment(G, "KA","K","A")
- AddSegment(G, "KL","K","L")
- AddSegment(G, "LK","L","K")
- AddSegment(G, "LF","L","F")
- return G
-print ("Probando el grafo...")
-G = CreateGraph_1 ()
+import matplotlib.pyplot as plt
+
+
+def CreateGraph_1():
+    G = Graph()
+    AddNode(G, Node("A", 1, 20))
+    AddNode(G, Node("B", 8, 17))
+    AddNode(G, Node("C", 15, 20))
+    AddNode(G, Node("D", 18, 15))
+    AddNode(G, Node("E", 2, 4))
+    AddNode(G, Node("F", 6, 5))
+    AddNode(G, Node("G", 12, 12))
+    AddNode(G, Node("H", 10, 3))
+    AddNode(G, Node("I", 19, 1))
+    AddNode(G, Node("J", 13, 5))
+    AddNode(G, Node("K", 3, 15))
+    AddNode(G, Node("L", 4, 10))
+
+    AddSegment(G, "AB", "A", "B")
+    AddSegment(G, "AE", "A", "E")
+    AddSegment(G, "AK", "A", "K")
+    AddSegment(G, "BA", "B", "A")
+    AddSegment(G, "BC", "B", "C")
+    AddSegment(G, "BF", "B", "F")
+    AddSegment(G, "BK", "B", "K")
+    AddSegment(G, "BG", "B", "G")
+    AddSegment(G, "CD", "C", "D")
+    AddSegment(G, "CG", "C", "G")
+    AddSegment(G, "DG", "D", "G")
+    AddSegment(G, "DH", "D", "H")
+    AddSegment(G, "DI", "D", "I")
+    AddSegment(G, "EF", "E", "F")
+    AddSegment(G, "FL", "F", "L")
+    AddSegment(G, "GB", "G", "B")
+    AddSegment(G, "GF", "G", "F")
+    AddSegment(G, "GH", "G", "H")
+    AddSegment(G, "ID", "I", "D")
+    AddSegment(G, "IJ", "I", "J")
+    AddSegment(G, "JI", "J", "I")
+    AddSegment(G, "KA", "K", "A")
+    AddSegment(G, "KL", "K", "L")
+    AddSegment(G, "LK", "L", "K")
+    AddSegment(G, "LF", "L", "F")
+    return G
+
+
+def CreateGraph_2():
+    G = Graph()
+    AddNode(G, Node("X", 5, 5))
+    AddNode(G, Node("Y", 10, 5))
+    AddNode(G, Node("Z", 7.5, 10))
+
+    AddSegment(G, "XY", "X", "Y")
+    AddSegment(G, "YZ", "Y", "Z")
+    AddSegment(G, "ZX", "Z", "X")
+    AddSegment(G, "YX", "Y", "X")
+    AddSegment(G, "ZY", "Z", "Y")
+    AddSegment(G, "XZ", "X", "Z")
+    return G
+
+
+print("Testing the graph...")
+G = CreateGraph_1()
 Plot(G)
 PlotNode(G, "C")
-n = GetClosest(G,15,5)
-print (n.name)
-n = GetClosest(G,8,19)
-print (n.name)
 
-def CreateGraph_2 ():
- G = Graph()
- AddNode(G, Node("A",1,20))
- AddNode(G, Node("B",8,17))
- AddNode(G, Node("C",15,20))
- AddNode(G, Node("D",18,15))
- AddNode(G, Node("E",2,4))
- AddNode(G, Node("F",6,5))
- AddNode(G, Node("G",12,12))
- AddNode(G, Node("H",10,3))
- AddNode(G, Node("I",19,1))
- AddNode(G, Node("J",13,5))
- AddNode(G, Node("K",3,15))
- AddNode(G, Node("L",4,10))
- AddSegment(G, "AB","A","B")
- AddSegment(G, "AE","A","E")
+n = GetClosest(G, 15, 5)
+print(n.name)  # Should print J
+n = GetClosest(G, 8, 19)
+print(n.name)  # Should print B
 
+# Test file operations
+SaveGraphToFile(G, "graph1.txt")
+loaded_graph = LoadGraphFromFile("graph1.txt")
+if loaded_graph:
+    Plot(loaded_graph)
 
-import graph
-
-
-def probar_grafo():
- g = graph.BuildGraphFromFile()
-
- print("Nodos cargados:")
- for node in g.nodes:
-  print(f"{node.name}: ({node.x}, {node.y})")
-
- print("\nSegmentos cargados:")
- for seg in g.segments:
-  print(f"{seg.name}: {seg.origin.name} -> {seg.destination.name} (costo: {seg.cost})")
-
- graph.Plot(g)
-
-
-if __name__ == "__main__":
- probar_grafo()
+# Test custom graph
+G2 = CreateGraph_2()
+Plot(G2)
