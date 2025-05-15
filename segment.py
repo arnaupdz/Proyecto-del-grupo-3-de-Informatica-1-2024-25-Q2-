07329d39d5783358f5e216f76f40fbe8bcf524f4
ferrannavarrete <ferrannavarretepiza@gmail.com>
2025-05-15 15:27:13 +0200

    Update segment.py
    VERSIÓN 3

--- segment.py
+++ segment.py
@@ -1,11 +1,21 @@
 from node import Distance
 
+
 class Segment:
-    def __init__(self, name, origin, destination):
+    def __init__(self, name, origin, destination, cost):
+        """
+        Constructor de la clase Segment
+
+        Args:
+            name (str): Nombre del segmento
+            origin (Node): Nodo de origen
+            destination (Node): Nodo de destino
+            cost (float): Costo del segmento (distancia)
+        """
         self.name = name
         self.origin = origin
         self.destination = destination
-        self.cost = Distance(origin, destination)
+        self.cost = cost
 
     def __str__(self):
         return f"{self.name}: {self.origin.name} -> {self.destination.name}, Cost: {self.cost:.2f}"
